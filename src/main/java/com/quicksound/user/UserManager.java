c542641a223134aa3b1f7f40e0edb831bb99b980
Mochaccin <mochaccindotcl@gmail.com>
2023-05-26 17:12:14 -0400

    elimine funciones sin uso UserManager

--- src/main/java/com/quicksound/user/UserManager.java
+++ src/main/java/com/quicksound/user/UserManager.java
@@ -22,22 +22,6 @@ public enum UserManager {
         return users.stream().anyMatch(user -> user.getName().equals(username));
     }
 
-    public void updateUserName(String username, int userId) {
-        if (!checkDuplicateUser(username)) {
-            users.get(userId).setUserName(username);
-        } else {
-            System.out.println("Ese nombre de usuario ya se encuentra registrado.");
-        }
-    }
-
-    public void updateUserPassword(String password, int userId) {
-        users.get(userId).setUserPassword(password);
-    }
-
-    public void updateUserId(int userId, int newUserId) {
-        users.get(userId).setUserId(newUserId);
-    }
-
     public void deleteUser(int userId) {
 
         if (!users.isEmpty()) {
@@ -52,30 +36,7 @@ public enum UserManager {
         }
         return users.size()-1;
     }
-
-    public void updateUsersIds(int index) {
-
-        if (!users.isEmpty() && index != users.size()-1) {
-            for (int i = index+1; i < users.size(); i++) {
-                updateUserId(i, i-1);
-            }
-        }
-    }
     public List<User> getUsers() {
         return users;
-    }
-    public User getLastUser() {
-
-        if (!users.isEmpty()) {
-            return users.get(users.size()-1);
-        } return null;
-    }
-    public void displayUsers() {
-
-        if (!users.isEmpty()) {
-            users.forEach(user -> System.out.println(user.toString()));
-        } else {
-            System.out.println("No hay usuarios registrados.");
-        }
     }
 }
